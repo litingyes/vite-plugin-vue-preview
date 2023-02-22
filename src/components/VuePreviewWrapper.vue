a24f11eb177f4d91222f5ef11df598e027a7b231
liting <luz.liting@gmail.com>
2023-02-23 00:34:30 +0800

    feat: hot update

--- src/components/VuePreviewWrapper.vue
+++ src/components/VuePreviewWrapper.vue
@@ -4,14 +4,18 @@
 
 <script lang="ts" setup>
 import { ReplStore, VuePreview } from './vue-repl'
-import { provide } from 'vue'
+import { provide, watch } from 'vue'
 
 export interface Props {
     code: string
 }
+
 const props = defineProps<Props>()
 
 const store = new ReplStore({ code: props.code });
 provide('store', store)
 
+watch(() => props.code, (val) => {
+    store.state.activeFile.code = val
+})
 </script>
