5a4d1e30d33811b2afe1565c14869a3c154e7ec5
liting <luz.liting@gmail.com>
2023-07-09 00:56:39 +0800

    chore: bump repl

--- src/VuePreview.vue
+++ src/VuePreview.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import '@liting-yes/vue-repl/style.css'
 import type { PreviewUpdateFlag, Store } from '@liting-yes/vue-repl'
-import { CodeMirror, Preview, ReplStore, defaultMainFile, importMapFile } from '@liting-yes/vue-repl'
-import { computed, onMounted, provide, ref } from 'vue'
-import { useClipboard, useDebounceFn, useElementHover } from '@vueuse/core'
+import { ExtendEditorContainer, Preview, ReplStore, defaultMainFile, importMapFile } from '@liting-yes/vue-repl'
+import { computed, onMounted, provide, ref, toRef } from 'vue'
+import { useClipboard, useElementHover } from '@vueuse/core'
 import Copy from './icons/Copy.vue'
 import Copied from './icons/Copied.vue'
 import UnfoldLess from './icons/UnfoldLess.vue'
@@ -29,6 +29,8 @@ export interface Props {
     }
   }
   importMap?: Record<string, string> | string
+  theme?: 'dark' | 'light'
+
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -46,6 +48,7 @@ const props = withDefaults(defineProps<Props>(), {
       useCode: '',
     },
   }),
+  theme: 'light',
 })
 
 const { store } = props
@@ -92,19 +95,16 @@ function onPreivewUpdatePreview(flag: PreviewUpdateFlag) {
 
 store.init()
 
-const onChange = useDebounceFn((code: string) => {
-  store.state.activeFile.code = code
-}, 250)
-
 provide('store', store)
 provide('autoresize', props.autoResize)
 provide('clear-console', props.clearConsole)
 provide('preview-options', props.previewOptions)
+provide('theme', toRef(props, 'theme'))
 
 const { copy, copied } = useClipboard({ source: store.state.activeFile.code, legacy: true })
 
 const isCollapse = ref(props.collapse)
-const maxHeightForCode = computed(() => isCollapse.value ? '0' : '1000px')
+const maxHeightForCode = computed(() => isCollapse.value ? '0' : 'var(--vue-preview-editor-max-height)')
 
 const previewBodyStyle = computed<Partial<CSSStyleDeclaration>>(() => typeof props.previewBodyStyle === 'string' ? JSON.parse(decodeURIComponent(props.previewBodyStyle)) : props.previewBodyStyle)
 const previewAppStyle = computed<Partial<CSSStyleDeclaration>>(() => typeof props.previewAppStyle === 'string' ? JSON.parse(decodeURIComponent(props.previewAppStyle)) : props.previewAppStyle)
@@ -139,7 +139,7 @@ const isHover = useElementHover(vuePreviewContainerRef)
         </div>
       </Transition>
     </div>
-    <CodeMirror :value="store.state.activeFile.code" @change="onChange" />
+    <ExtendEditorContainer />
   </div>
 </template>
 
@@ -151,11 +151,18 @@ const isHover = useElementHover(vuePreviewContainerRef)
   --vue-preview-color-icon: hsl(220, 13%, 18%);
   --vue-preview-color-icon-bg-hover: hsl(220, 95%, 95%);
   --vue-preview-color-model-bg: hsla(0, 0%, 80%, 0.1);
+  --vue-preview-editor-max-height: 50vh;
 }
 </style>
 
 <style lang="scss" scoped>
 .vue-preview {
+  --bg: #fff;
+  --border: #ddd;
+  --text-light: #888;
+  --font-code: Menlo, Monaco, Consolas, 'Courier New', monospace;
+  --color-branding: #42b883;
+
   width: 100%;
   border-radius: var(--vue-preview-radius);
   overflow: hidden;
@@ -222,14 +229,25 @@ const isHover = useElementHover(vuePreviewContainerRef)
     background-color: var(--vue-preview-color-model-bg);
   }
 
+  .editor-container {
+    height: auto;
+  }
+
   :deep(.editor) {
     box-sizing: border-box;
-    overflow: hidden;
-    height: auto;
+    overflow-y: scroll;
     border-radius: 0 0 var(--vue-preview-radius) var(--vue-preview-radius);
     max-height: v-bind('maxHeightForCode');
     transition: max-height 0.3s;
   }
+}
+
+.dark .vue-preview {
+  --bg: #1a1a1a;
+  --border: #383838;
+  --text-light: #aaa;
+  --color-branding: #42d392;
+  --color-branding-dark: #89ddff;
 }
 
 .vue-preview-slide-down-enter-active,
--- test/vue/src/DemoSfc.vue
+++ test/vue/src/DemoSfc.vue
@@ -1,25 +1,11 @@
 <script lang="ts" setup>
 // import '../../../dist/style.css'
 import { VuePreview } from '../../../src'
-
-const code = encodeURIComponent(`
-<script setup>
-import { isString } from '@vue/shared'
-
-const res = isString('@vue/shared')
-<\/script>
-
-<template>
-  <h1>Test ImportMap</h1>
-  <div>Result: {{res}}</div>
-</template>
-`.trim(),
-)
 </script>
 
 <template>
   <div class="demo-sfc">
-    <VuePreview :code="code" :clear-console="false" encode :import-map="{ '@vue/shared': 'https://unpkg.com/@vue/shared@latest/dist/shared.esm-bundler.js' }" />
+    <VuePreview />
   </div>
 </template>
 
